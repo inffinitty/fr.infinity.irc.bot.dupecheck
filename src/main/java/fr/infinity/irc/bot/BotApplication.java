e52f70fcb3c867bed01a285baada18493a04c996
inffinitty <103312778+inffinitty@users.noreply.github.com>
2022-04-09 13:08:41 +0200

    Update BotApplication.java

--- src/main/java/fr/infinity/irc/bot/BotApplication.java
+++ src/main/java/fr/infinity/irc/bot/BotApplication.java
@@ -1,6 +1,6 @@
 package fr.infinity.irc.bot;
 
-import fr.infinity.irc.bot.oracle.service.BotService;
+import fr.infinity.irc.bot.service.BotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
