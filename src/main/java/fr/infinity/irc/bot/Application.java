a074b8cda24460c4d64874cb1cd2024c556017bb
inffinitty <103312778+inffinitty@users.noreply.github.com>
2022-04-09 12:45:09 +0200

    Add files via upload

---
+++ src/main/java/fr/infinity/irc/bot/Application.java
@@ -0,0 +1,27 @@
+package fr.infinity.irc.bot.oracle;
+
+import fr.infinity.irc.bot.oracle.service.BotService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Application implements CommandLineRunner {
+
+
+    private static BotService botService;
+
+    @Autowired
+    public Application(BotService botService) {
+        this.botService = botService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        execute(args);
+    }
+
+    public static void execute(String[] args) {
+        botService.execute();
+    }
+}
